b8ecacb7ae7dade4477c2a355d5e8a56edb560a1
pouRia <pouria.f8410@gmail.com>
2024-05-28 12:26:37 +0330

    profile model in binded to user model using Django Signals

---
+++ accounts/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from accounts.models import User, UserProfile
+
+
+@receiver(post_save, sender=User)
+def post_save_create_profile_receiver(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        profile = UserProfile.objects.get_or_create(user=instance)
+        profile.save()
